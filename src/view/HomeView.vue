d9ac4fd9637915de11fad182636caf510c4ec5b5
John Jacob Dimaya <johnjacobdimaya0@gmail.com>
2025-07-25 15:06:30 +0800

    fix typo

--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Hero from '@/components/Hero.vue'
 import HomeCard from '@/components/HomeCard.vue'
-import JobListings from '@/components/JobListings.vue'
+import JobListings from '@/components/JobLIstings.vue'
 </script>
 
 <template>
